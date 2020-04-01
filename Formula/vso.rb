b530c5bf16c726df5a9a7478c848f7ffe74f6c1a
Derek Bekoe <debekoe@microsoft.com>
2020-04-01 11:49:12 -0700

    Requirement updates

--- Formula/vso.rb
+++ Formula/vso.rb
@@ -1,4 +1,5 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
 
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
