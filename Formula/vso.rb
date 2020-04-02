4894cfbe54b3f90cc26b0165c68a655c3b2b94c0
Derek Bekoe <derek@derekbekoe.com>
2020-04-02 11:14:30 -0700

    Update vso formula to version 3604372 (#4)
    * Update vso formula to version 3604372

--- Formula/vso.rb
+++ Formula/vso.rb
@@ -1,15 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
-
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class Vso < Formula
   desc "Visual Studio Online Self-Hosted Agent"
   homepage "https://online.visualstudio.com"
   # We do not specify `version "..."` as 'brew audit' will complain - see https://github.com/Homebrew/legacy-homebrew/issues/32540
-  url "https://vsoagentdownloads.blob.core.windows.net/vsoagent/VSOAgent_osx_3552852.zip"
+  url "https://vsoagentdownloads.blob.core.windows.net/vsoagent/VSOAgent_osx_3604372.zip"
   # must be lower-case
-  sha256 "3dc62ded13434d8302d8d31612a1f99dc91d484f033fe3c5ca63c9baf58d8a8e"
+  sha256 "fc27cefecf7d7a0c4a32a0a466a83df494b52a7a861b19b2dc6a132c38ca68e0"
   bottle :unneeded
 
   # .NET Core 3.1 requires High Sierra - https://docs.microsoft.com/en-us/dotnet/core/install/dependencies?pivots=os-macos&tabs=netcore31
